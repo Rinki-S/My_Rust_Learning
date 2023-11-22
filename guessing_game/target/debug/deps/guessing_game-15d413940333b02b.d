738268c915eef36c57338e95af2448d93bdf6f18
riiiinki <nichijousong08@gmail.com>
2023-11-22 16:54:02 +0800

    Fix bug in login functionality

---
+++ guessing_game/target/debug/deps/guessing_game-15d413940333b02b.d
@@ -0,0 +1,5 @@
+/Users/rinki/Library/CloudStorage/OneDrive-whut.edu.cn/Coding/Rust/guessing_game/target/debug/deps/libguessing_game-15d413940333b02b.rmeta: src/main.rs
+
+/Users/rinki/Library/CloudStorage/OneDrive-whut.edu.cn/Coding/Rust/guessing_game/target/debug/deps/guessing_game-15d413940333b02b.d: src/main.rs
+
+src/main.rs:
---
+++ guessing_game/target/debug/deps/guessing_game-acdeef1a6ed95073.d
@@ -0,0 +1,5 @@
+/Users/rinki/Library/CloudStorage/OneDrive-whut.edu.cn/Coding/Rust/guessing_game/target/debug/deps/libguessing_game-acdeef1a6ed95073.rmeta: src/main.rs
+
+/Users/rinki/Library/CloudStorage/OneDrive-whut.edu.cn/Coding/Rust/guessing_game/target/debug/deps/guessing_game-acdeef1a6ed95073.d: src/main.rs
+
+src/main.rs:
